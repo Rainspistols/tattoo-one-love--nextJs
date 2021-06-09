1917252c4375f1e1f58d3ab204e2771fa01f3c8e
andrezej <a.husiev@fudosecurity.com>
2021-06-09 16:21:44 +0200

    Add 'goal' section, add textwrapper styled component with inputs to control paddings background and borders.

--- pages/coverups.jsx
+++ pages/coverups.jsx
@@ -21,15 +21,53 @@ const Coverups = () => {
             width="665"
             height="664"
           />
-          <Text>
-            Tatuaże powinni pomagać wyrażać siebie i dodawać wiary w siebie, a
-            nie na odwrót. Ale niestety, czasem tak się zdarza, że wymarzony
-            tatuaż, z różnych powodów, nie spełnia oczekiwań. Patrzenie na
-            nieudany wzór, który szpeci ciało, jest krępujące, a niekiedy wręcz
-            żenujące. Więc postanowiliśmy pomóc ludziom cieszyć się rysunkami na
-            ich ciałach, ponieważ wszyscy na to zasługują.
-          </Text>
+          <TextWrapper withBg>
+            <Text>
+              Tatuaże powinni pomagać wyrażać siebie i dodawać wiary w siebie, a
+              nie na odwrót. Ale niestety, czasem tak się zdarza, że wymarzony
+              tatuaż, z różnych powodów, nie spełnia oczekiwań. Patrzenie na
+              nieudany wzór, który szpeci ciało, jest krępujące, a niekiedy
+              wręcz żenujące. Więc postanowiliśmy pomóc ludziom cieszyć się
+              rysunkami na ich ciałach, ponieważ wszyscy na to zasługują.
+            </Text>
+          </TextWrapper>
         </DescriptionContainer>
+
+        <GoalSection>
+          <TextWrapper
+            withBorder
+            borderPos="top"
+            setPadding
+            pos="bottom"
+            value="0"
+          >
+            <Text>
+              Źle wykonane tatuaże mają bardzo negatywny wpływ na postrzeganie
+              branży tatuażu. Widząc nieudane tatuaże ludzie boją się ozdabiać
+              swoje ciało, ponieważ nie ma żadnej gwarancji, że tatuaże będą
+              wyglądał tak, jak oni chcą. Planujemy zmienić sytuację - chcemy
+              zmniejszyć ilość niechcianych i nieudanych tatuaży na świecie, i
+              zwiększyć ilość szczęśliwych posiadaczów wymarzonych dziar.
+            </Text>
+          </TextWrapper>
+
+          <TextWrapper setPadding pos="top" value="20">
+            <Text>
+              Pragniemy też pomóc ludziom zakryć tatuaże związane z rasizmem,
+              ksenofobią itd. Każdy musi mieć szansę na zmianę. Tatuaż powinien
+              przynosić tylko te emocje, które osoba w niego wkłada, ale na
+              pewno nie wstyd, rozczarowanie czy irytację.
+            </Text>
+          </TextWrapper>
+
+          <TextWrapper withBorder borderPos="top">
+            <Text>
+              Dlatego my jako studio tatuażu i jako przedstawiciele całej branży
+              tatuatorskiej ogłaszamy początek naszego projektu socjalnego
+              „Cover to recover”, bo niechciany tatuaż nie musi Cię określać.
+            </Text>
+          </TextWrapper>
+        </GoalSection>
       </Container>
     </>
   );
@@ -46,17 +84,43 @@ const DescriptionContainer = styled.div`
   }
 `;
 
+const TextWrapper = styled.div`
+  padding: ${({ theme }) => theme.vw(32)} ${({ theme }) => theme.vw(16)};
+
+  ${({ theme }) => theme.mediaDesktop} {
+    padding: ${({ theme }) => theme.vw1920(117)}
+      ${({ theme }) => theme.vw1920(64)};
+  }
+
+  ${({ withBorder, borderPos }) => {
+    return withBorder && `border-${borderPos}: 1px solid #D8E2EB`;
+  }}
+
+  ${({ withBg }) => {
+    return withBg && "background-color: #f4f4f4";
+  }};
+
+  ${({ setPadding, pos, value }) => {
+    return setPadding && `padding-${pos}: ${value}px !important`;
+  }};
+`;
+
 const Text = styled.p`
-  padding: 64px 32px;
-  background: #f4f4f4;
   font-weight: 400;
   font-size: ${({ theme }) => theme.vw(13)};
   line-height: ${({ theme }) => theme.vw(20)};
 
   ${({ theme }) => theme.mediaDesktop} {
-    padding: 117px 64px;
-    font-size: ${(props) => props.theme.vw1920(29)};
-    line-height: ${(props) => props.theme.vw1920(43)};
+    font-size: ${({ theme }) => theme.vw1920(29)};
+    line-height: ${({ theme }) => theme.vw1920(43)};
+  }
+`;
+
+const GoalSection = styled.div`
+  padding: ${({ theme }) => theme.vw(20)};
+
+  ${({ theme }) => theme.mediaDesktop} {
+    padding: ${({ theme }) => theme.vw1920(90)};
   }
 `;
 
