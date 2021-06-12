57e54aa345332eded2ebb25a216e8f3baa19a245
andrezej <a.husiev@fudosecurity.com>
2021-06-12 09:00:02 +0200

    Make CoverupsDescription separate independent component

--- components/Coverups/CoverupsDescription.jsx
+++ components/Coverups/CoverupsDescription.jsx
@@ -1,30 +1,29 @@
 import styled from "@emotion/styled";
 import Image from "next/image";
-import { TextWrapper, Text } from "~/pages/coverups";
 
 const CoverupsDescription = () => {
   return (
     <DescriptionContainer>
-      <Image
-        src="/Coverups/coverup-description.jpeg"
-        width="665"
-        height="664"
-      />
-      <TextWrapper withBg>
-        <Text>
-          Tatuaże powinni pomagać wyrażać siebie i dodawać wiary w siebie, a nie
-          na odwrót. Ale niestety, czasem tak się zdarza, że wymarzony tatuaż, z
-          różnych powodów, nie spełnia oczekiwań. Patrzenie na nieudany wzór,
-          który szpeci ciało, jest krępujące, a niekiedy wręcz żenujące. Więc
-          postanowiliśmy pomóc ludziom cieszyć się rysunkami na ich ciałach,
-          ponieważ wszyscy na to zasługują.
-        </Text>
-      </TextWrapper>
+      <div className="description-image-wrapper">
+        <Image
+          src="/Coverups/coverup-description.jpeg"
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+      <p className="description-content">
+        Tatuaże powinni pomagać wyrażać siebie i dodawać wiary w siebie, a nie
+        na odwrót. Ale niestety, czasem tak się zdarza, że wymarzony tatuaż, z
+        różnych powodów, nie spełnia oczekiwań. Patrzenie na nieudany wzór,
+        który szpeci ciało, jest krępujące, a niekiedy wręcz żenujące. Więc
+        postanowiliśmy pomóc ludziom cieszyć się rysunkami na ich ciałach,
+        ponieważ wszyscy na to zasługują.
+      </p>
     </DescriptionContainer>
   );
 };
 
-const DescriptionContainer = styled.div`
+const DescriptionContainer = styled.article`
   display: grid;
   grid-template-columns: 1fr;
   align-items: center;
@@ -32,6 +31,32 @@ const DescriptionContainer = styled.div`
 
   ${({ theme }) => theme.mediaDesktop} {
     grid-template-columns: auto 1fr;
+  }
+
+  .description-image-wrapper {
+    position: relative;
+    width: ${({ theme }) => theme.vw(375)};
+    height: ${({ theme }) => theme.vw(375)};
+
+    ${({ theme }) => theme.mediaDesktop} {
+      position: relative;
+      width: ${({ theme }) => theme.vw1920(665)};
+      height: ${({ theme }) => theme.vw1920(664)};
+    }
+  }
+
+  .description-content {
+    font-size: ${({ theme }) => theme.vw(29)};
+    line-height: ${({ theme }) => theme.vw(43)};
+    padding: ${({ theme }) => theme.vw(32)} ${({ theme }) => theme.vw(16)};
+    background-color: ${({ theme }) => theme.colors.lightGrey};
+
+    ${({ theme }) => theme.mediaDesktop} {
+      font-size: ${({ theme }) => theme.vw1920(29)};
+      line-height: ${({ theme }) => theme.vw1920(43)};
+      padding: ${({ theme }) => theme.vw1920(117)}
+        ${({ theme }) => theme.vw1920(64)};
+    }
   }
 `;
 
