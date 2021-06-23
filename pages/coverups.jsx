68cf789a948c9c45b03a67ac5baacef77100f13d
andrezej <a.husiev@fudosecurity.com>
2021-06-23 12:51:23 +0200

    Add new section for description coverups page, placed on the top of this page.

--- pages/coverups.jsx
+++ pages/coverups.jsx
@@ -1,7 +1,9 @@
 import { NextSeo } from 'next-seo';
 import Hero, { heroTypes } from '~/components/Hero/Hero';
+import Container from '~/Layouts/Container/Container';
+import CoverupsTopDescription from '~/components/Coverups/CoverupsTopDescription';
 
-const NewPage = () => (
+const Coverups = () => (
   <>
     <NextSeo title="Coverups | Tattoo One Love" />
     <h1 className="visually-hidden">cover tatuażu</h1>
@@ -10,7 +12,10 @@ const NewPage = () => (
       text1="projekt socjalny: cover to recover"
       text2="Niechciany tatuaż nie musi Cię definiować."
     />
+    <Container>
+      <CoverupsTopDescription />
+    </Container>
   </>
 );
 
-export default NewPage;
+export default Coverups;
