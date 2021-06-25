064b477387a686781a54e17c63808d0009c51649
andrezej <a.husiev@fudosecurity.com>
2021-06-25 15:50:34 +0200

    Move styles for paragraph from own styled component to parent styled component.

--- components/Coverups/CoverupsGoal.jsx
+++ components/Coverups/CoverupsGoal.jsx
@@ -19,7 +19,7 @@ const CoverupsGoal = () => {
   return (
     <GoalSection>
       {data.map(({ id, text }) => (
-        <GoalSectionContentStyled key={id}>{text}</GoalSectionContentStyled>
+        <p key={id}>{text}</p>
       ))}
     </GoalSection>
   );
@@ -31,24 +31,24 @@ const GoalSection = styled.section`
   ${({ theme }) => theme.mediaDesktop} {
     padding: ${({ theme }) => theme.vw1920(90)};
   }
-`;
 
-const GoalSectionContentStyled = styled.p`
-  font-weight: 400;
-  font-size: ${({ theme }) => theme.vw(13)};
-  line-height: ${({ theme }) => theme.vw(20)};
-  padding: ${({ theme }) => theme.vw(32)} ${({ theme }) => theme.vw(16)};
+  p {
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.vw(13)};
+    line-height: ${({ theme }) => theme.vw(20)};
+    padding: ${({ theme }) => theme.vw(32)} ${({ theme }) => theme.vw(16)};
 
-  ${({ theme }) => theme.mediaDesktop} {
-    font-size: ${({ theme }) => theme.vw1920(29)};
-    line-height: ${({ theme }) => theme.vw1920(43)};
-    padding: ${({ theme }) => theme.vw1920(117)} ${({ theme }) => theme.vw1920(64)};
-  }
+    ${({ theme }) => theme.mediaDesktop} {
+      font-size: ${({ theme }) => theme.vw1920(29)};
+      line-height: ${({ theme }) => theme.vw1920(43)};
+      padding: ${({ theme }) => theme.vw1920(117)} ${({ theme }) => theme.vw1920(64)};
+    }
 
-  border-top: 1px solid ${({ theme }) => theme.colors.lightBlue};
+    border-top: 1px solid ${({ theme }) => theme.colors.lightBlue};
 
-  &:last-child {
-    padding-bottom: 0;
+    &:last-child {
+      padding-bottom: 0;
+    }
   }
 `;
 
