7c2a56a09a1b39c383f20db1d987229df2d44006
andrezej <a.husiev@fudosecurity.com>
2021-06-25 12:04:36 +0200

    Add new section to Coverups page which describe a goal of this page

--- pages/coverups.jsx
+++ pages/coverups.jsx
@@ -1,7 +1,9 @@
+
 import { NextSeo } from 'next-seo';
 import Hero, { heroTypes } from '~/components/Hero/Hero';
 import Container from '~/Layouts/Container/Container';
 import TopPageDescription from '~/UI/TopPageDescription';
+import CoverupsGoal from '~/components/Coverups/CoverupsGoal';
 
 const Coverups = () => (
   <>
@@ -18,6 +20,7 @@ const Coverups = () => (
         imageDescription='cover tatuaż'
         textContent='Tatuaże powinni pomagać wyrażać siebie i dodawać wiary w siebie, a nie na odwrót. Ale niestety, czasem tak się zdarza, że wymarzony tatuaż, z różnych powodów, nie spełnia oczekiwań. Patrzenie na nieudany wzór, który szpeci ciało, jest krępujące, a niekiedy wręcz żenujące. Więc postanowiliśmy pomóc ludziom cieszyć się rysunkami na ich ciałach, ponieważ wszyscy na to zasługują.'
       />
+      <CoverupsGoal />
     </Container>
   </>
 );
